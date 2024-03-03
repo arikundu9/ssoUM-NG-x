d5a1ee10856beb01717d3196d83356b3a07d0b26
Arijit Kundu <arikundu9@gmail.com>
2024-03-04 02:03:21 +0530

    +   import '@material/web/button/filled-button.js';

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import '@material/web/button/filled-button.js';
 
 if (environment.production) {
     enableProdMode();
