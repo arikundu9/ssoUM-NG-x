b78a21ac9f6ee8c5bdefbe6e0afc833c1b01a1a3
Arijit Kundu <arikundu9@gmail.com>
2024-03-04 02:08:28 +0530

    mat3 working

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import '@material/web/button/filled-button.js';
+import '@material/web/button/elevated-button.js';
 
 if (environment.production) {
     enableProdMode();
